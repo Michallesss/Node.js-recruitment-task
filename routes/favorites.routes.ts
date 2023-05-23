d7f98b6b2810bdc7f24c1da4fb635f3017ea689d
Michallesss <praktykant@imoli.dev>
2023-05-23 07:57:37 +0200

    ✨ new routes

--- routes/favorites.routes.ts
+++ routes/favorites.routes.ts
@@ -3,115 +3,195 @@ const router=express.Router();
 export default router;
 import axios, { AxiosResponse } from 'axios';
 import { QueryResult } from 'pg';
-import client from '../app';
+import { client } from '../app';
+import xlsx from 'xlsx';
 
 
 router.get('/', async (_req: Request, _res: Response) => { // TODO: make it works
-    let [favoritesResult, favoritesError]: any=await client.query(`SELECT *, '' as characters FROM favorites`)
-    .then((res: QueryResult) => [res.rows, null])
-    .catch((err: Error) => [null, err]);
-    if(favoritesError) return _res.status(500).json({
-        "status":"error",
-        "message":"500 internal server error",
-    });
-    if(!favoritesResult) return _res.status(404).json({
-        "status":"error",
-        "message":"404 not found",
-    });
-    
-    await favoritesResult.forEach(async (fav: any, index: number) => {
-        const [charactersResult, charactersError]: any=await client.query(`SELECT characters.link FROM characters JOIN characters_favorites ON characters_favorites.characters_id=characters.id JOIN favorites ON favorites.id=characters_favorites.favorites_id WHERE favorites.id=${fav.id}`)
+    try {
+        let [favoritesResult, favoritesError]: any=await client.query(`SELECT *, '' as characters FROM favorites`)
         .then((res: QueryResult) => [res.rows, null])
         .catch((err: Error) => [null, err]);
-        if(charactersError) return _res.status(500).json({
-            "status":"error",
-            "message":"500 internal server error",
+        if(favoritesError) return _res.status(500).json({
+            status:"error",
+            message:"500 internal server error",
         });
-        if(!charactersResult) return _res.status(404).json({
-            "status":"error",
-            "message":"404 not found",
+        if(!favoritesResult) return _res.status(404).json({
+            status:"error",
+            message:"404 not found",
         });
-
-        // fav['characters']=charactersResult; // ! DOESN'T UPDATEEEE 
-        // favoritesResult[index]['characters']=charactersResult; // ? WHY IT DOESN'T UPDATE
-        // await console.log(fav); // * test
-    });
+        
+        await favoritesResult.forEach(async (fav: any, index: number) => {
+            const [charactersResult, charactersError]: any=await client.query(`SELECT characters.link FROM characters JOIN characters_favorites ON characters_favorites.characters_id=characters.id JOIN favorites ON favorites.id=characters_favorites.favorites_id WHERE favorites.id=${fav.id}`)
+            .then((res: QueryResult) => [res.rows, null])
+            .catch((err: Error) => [null, err]);
+            if(charactersError) return _res.status(500).json({
+                status:"error",
+                message:"500 internal server error",
+            });
+            if(!charactersResult) return _res.status(404).json({
+                status:"error",
+                message:"404 not found",
+            });
     
-    // await console.log(favoritesResult); // * test
-    return _res.status(200).json({
-        "status": "success",
-        "message": "mamy to",
-        "date": favoritesResult,
-    });
+            // fav['characters']=charactersResult; // ! DOESN'T UPDATEEEE 
+            // favoritesResult[index]['characters']=charactersResult; // ? WHY IT DOESN'T UPDATE
+            // await console.log(fav); // * test lol
+        });
+        
+        // console.log(favoritesResult); // * test
+        return _res.status(200).json({
+            status: "success",
+            message: "mamy to",
+            data: favoritesResult,
+        });
+    } catch(error) {
+        console.error(error)
+        _res.status(500).json({
+            status: "error",
+            message: "Internal server error 500",
+        });
+    }
 });
 
 router.get('/:id', async (_req: Request, _res: Response) => {
-    const [ favoritesResult, favoritesError ]: any=await client.query(`SELECT *, '' as characters FROM favorites WHERE id=${_req.params.id}`)
-    .then((res) => [res.rows, null])
-    .catch((err) => [null, err]);
-    if(favoritesError) return _res.status(500).json({
-        "status":"error",
-        "message":"500 internal server error",
-    });
-    if(!favoritesResult) return _res.status(404).json({
-        "status":"error",
-        "message":"404 not found",
-    });
-
-    const [ charactersResult, charactersError ]: any=await client.query(`SELECT characters.link FROM characters JOIN characters_favorites ON characters_favorites.characters_id=characters.id JOIN favorites ON favorites.id=characters_favorites.favorites_id WHERE favorites.id=${_req.params.id}`)
-    .then((res) => [res.rows, null])
-    .catch((err) => [null, err]);
-    if(charactersError) return _res.status(500).json({
-        "status":"error",
-        "message":"500 internal server error",
-    });
-    if(!charactersResult) return _res.status(404).json({
-        "status":"error",
-        "message":"404 not found",
-    });
-
-    return _res.status(200).json({
-        status: "success",
-        message: "mamy to",
-        data: {
-            film: favoritesResult,
-            characters: charactersResult,
-        },
-    });
+    try {
+        const [ favoritesResult, favoritesError ]: any=await client.query(`SELECT *, '' as characters FROM favorites WHERE id=${_req.params.id}`)
+        .then((res) => [res.rows, null])
+        .catch((err) => [null, err]);
+        if(favoritesError) return _res.status(500).json({
+            status:"error",
+            message:"500 internal server error",
+        });
+        if(!favoritesResult) return _res.status(404).json({
+            status:"error",
+            message:"404 not found",
+        });
+    
+        const [ charactersResult, charactersError ]: any=await client.query(`SELECT characters.link FROM characters JOIN characters_favorites ON characters_favorites.characters_id=characters.id JOIN favorites ON favorites.id=characters_favorites.favorites_id WHERE favorites.id=${_req.params.id}`)
+        .then((res) => [res.rows, null])
+        .catch((err) => [null, err]);
+        if(charactersError) return _res.status(500).json({
+            status:"error",
+            message:"500 internal server error",
+        });
+        if(!charactersResult) return _res.status(404).json({
+            status:"error",
+            message:"404 not found",
+        });
+    
+        return _res.status(200).json({
+            status: "success",
+            message: "mamy to",
+            data: {
+                film: favoritesResult,
+                characters: charactersResult,
+            },
+        });
+    } catch(error) {
+        console.error(error)
+        _res.status(500).json({
+            status: "error",
+            message: "Internal server error 500",
+        });
+    }
 });
 
 router.get('/:id/file ', async (_req: Request, _res: Response) => {
-    const [ favoritesResult, favoritesError ]: any=await client.query(`SELECT *, '' as characters FROM favorites WHERE id=${_req.params.id}`)
-    .then((res) => [res.rows, null])
-    .catch((err) => [null, err]);
-    if(favoritesError) return _res.status(500).json({
-        "status":"error",
-        "message":"500 internal server error",
-    });
-    if(!favoritesResult) return _res.status(404).json({
-        "status":"error",
-        "message":"404 not found",
-    });
-
-    const [ charactersResult, charactersError ]: any=await client.query(`SELECT characters.link FROM characters JOIN characters_favorites ON characters_favorites.characters_id=characters.id JOIN favorites ON favorites.id=characters_favorites.favorites_id WHERE favorites.id=${_req.params.id}`)
-    .then((res) => [res.rows, null])
-    .catch((err) => [null, err]);
-    if(charactersError) return _res.status(500).json({
-        "status":"error",
-        "message":"500 internal server error",
-    });
-    if(!charactersResult) return _res.status(404).json({
-        "status":"error",
-        "message":"404 not found",
-    });
-
-    // TODO: make it excel file
-});
-
-router.post('/', async (_req: Request, _res: Response) => 
-{
-    const value: string | number=await _req.body.id || _req.body.movie;
-    const film: AxiosResponse=await axios.get(`https://swapi.dev/api/films/${value}`);
+    try {
+        const [ favoritesResult, favoritesError ]: any=await client.query(`SELECT * FROM favorites WHERE id=${_req.params.id}`)
+        .then((res) => [res.rows, null])
+        .catch((err) => [null, err]);
+        if(favoritesError) return _res.status(500).json({
+            status:"error",
+            message:"500 internal server error",
+        });
+        if(!favoritesResult) return _res.status(404).json({
+            status:"error",
+            message:"404 not found",
+        });
     
-    // ...
+        const [ charactersResult, charactersError ]: any=await client.query(`SELECT characters.link FROM characters JOIN characters_favorites ON characters_favorites.characters_id=characters.id JOIN favorites ON favorites.id=characters_favorites.favorites_id WHERE favorites.id=${_req.params.id}`)
+        .then((res) => [res.rows, null])
+        .catch((err) => [null, err]);
+        if(charactersError) return _res.status(500).json({
+            status:"error",
+            message:"500 internal server error",
+        });
+        if(!charactersResult) return _res.status(404).json({
+            status:"error",
+            message:"404 not found",
+        });
+
+        const data=[
+            favoritesResult,
+            charactersResult,
+        ];
+
+        const worksheet=xlsx.utils.json_to_sheet(data);
+        const workBook=xlsx.utils.book_new();
+
+        xlsx.utils.book_append_sheet(workBook,worksheet,'favorites');
+        xlsx.write(workBook, {
+            bookType:'xlsx',
+            type:'buffer'
+        });
+
+        xlsx.write(workBook, {
+            bookType:'xlsx',
+            type:'binary'
+        });
+
+        xlsx.writeFile(workBook,'favorites.xlsx');
+
+        return _res.status(200).json({
+            status: "success",
+            message: "Excel file exported",
+        });
+    } catch(error) {
+        console.error(error)
+        _res.status(500).json({
+            status: "error",
+            message: "Internal server error 500",
+        });
+    }
+}); 
+
+router.post('/', async (_req: Request, _res: Response) => {
+    const value: string | number = _req.body.query;
+    try
+    {
+        let result: AxiosResponse | any;
+        typeof(_req.body.query) === 'string' ? result = await axios.get(`https://swapi.dev/api/films?search=${value}`) : result = await axios.get(`https://swapi.dev/api/films/${value}`);
+        typeof(_req.body.query) === 'string' ? result = result.data.results[0] : result = result.data;
+        let data: [number, string, string] = [result.episode_id,result.title,result.release_date];
+        let query = await client.query(`SELECT id FROM favorites where id = (${data[0]}::int)`).then(res => res.rows[0]);
+        if(query === undefined)
+        {
+            client.query(`insert into favorites("id", "title", "releaseDate") values(${data[0]}, '${data[1]}', '${data[2]}');`)
+            _res.status(200).json({
+                status: "success",
+                query: value,
+                result: data
+            });
+        }
+        else
+        {
+            _res.status(400).json({
+                status: "error",
+                message: "bad request | list already in the database",
+                query: value
+            });
+        }
+    }
+    catch(err)
+    {
+        console.error(err)
+        _res.status(404).json({
+            status: "error",
+            message: "list does not exists",
+        });
+    }
 });
+// d(-_-)b
+// :* :D :> :P :O :V :S 
--- routes/films.routes.ts
+++ routes/films.routes.ts
@@ -5,22 +5,38 @@ import axios, { AxiosResponse } from 'axios';
 import Film from '../interfaces/interfaces'
 
 router.get('/', async (_req: Request, _res: Response) => {
-    const result: AxiosResponse = await axios.get('https://swapi.dev/api/films/');
-    const films: [Film] = result.data;
-    return _res.status(200).json({
-        "status": "success",
-        "message": "mamy to",
-        "data": films
-    });
+    try {
+        const result: AxiosResponse = await axios.get('https://swapi.dev/api/films/');
+        const films: [Film] = result.data;
+        return _res.status(200).json({
+            status: "success",
+            message: "mamy to",
+            data: films
+        });
+    } catch(error) {
+        console.error(error)
+        _res.status(500).json({
+            status: "error",
+            message: "Internal server error 500",
+        });
+    }
 });
 
 router.get('/:id', async (_req: Request, _res: Response) => {
-    const id: string = _req.params.id;
-    const result: AxiosResponse = await axios.get(`https://swapi.dev/api/films/${id}/`);
-    const films: [Film] = result.data;
-    return _res.status(200).json({
-        "status": "success",
-        "message": "mamy to",
-        "data": films
-    });
+    try {
+        const id: string = _req.params.id;
+        const result: AxiosResponse = await axios.get(`https://swapi.dev/api/films/${id}/`);
+        const films: [Film] = result.data;
+        return _res.status(200).json({
+            status: "success",
+            message: "mamy to",
+            data: films
+        });
+    } catch(error) {
+        console.error(error)
+        _res.status(500).json({
+            status: "error",
+            message: "Internal server error 500",
+        });
+    }
 });
